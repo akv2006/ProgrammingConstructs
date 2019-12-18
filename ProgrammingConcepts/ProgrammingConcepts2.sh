feb548180663664a38d775ac7f2efea8956de175
akv2006 <akv_2006@yahoo.co.in>
2019-12-18 16:31:26 +0530

    Did correction in 2nd problem of Programming concepts

--- ProgrammingConcepts/ProgrammingConcepts2.sh
+++ ProgrammingConcepts/ProgrammingConcepts2.sh
@@ -5,4 +5,7 @@ a=10
 b=20
 c=30
 
-echo $(($a+$b*$c))
+echo a + b \* c = $(($a + $b * $c))
+echo a % b + c = $(($a % $b + $c))
+echo c + a / b = $(($c + $a / $b))
+echo a \* b + c = $(($a * $b + $c))
