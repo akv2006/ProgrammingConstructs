6b0f10d8da08f41bc8f5c6c22954a5a673a604bc
akv2006 <akv_2006@yahoo.co.in>
2019-12-18 16:23:46 +0530

    Completed problem 4th of elif selection

---
+++ ElifSelection/ElIf4.sh
@@ -0,0 +1,39 @@
+#!/usr/local/bin/bash -x
+
+#Variables
+read a
+read b
+read c
+
+echo 'a + b * c = ' $(($a + $b * $c))
+echo 'a % b + c = ' $(($a % $b + $c))
+echo 'c + a / b = ' $(($c + $a / $b))
+echo 'a * b + c = ' $(($a * $b + $c))
+
+max=$a
+if [ $b -gt $max ]
+then
+	max=$b
+fi
+
+if [ $c -gt $max ]
+then
+	max=$c
+fi
+
+echo Max Num is $max
+
+
+
+min=$a
+if [ $b -lt $min ]
+then
+   min=$b
+fi
+
+if [ $c -lt $min ]
+then
+   min=$c
+fi
+
+echo Min Num is $min
