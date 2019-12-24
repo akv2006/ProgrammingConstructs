0d3fe612e81b0f343b8146bc35a02c2dd64ea34e
akv2006 <akv_2006@yahoo.co.in>
2019-12-24 10:30:44 +0530

    Did correction in program 3 of for loops

--- ForLoop/for3.sh
+++ ForLoop/for3.sh
@@ -2,26 +2,35 @@
 
 #Take user input
 
-read n
+read -p "Enter a number: " n
 
-# Check if entered number is prime
+##Check if n is 1, 2 or 3
+
 if [ $n -eq 1 ] 
 then 
-	echo Not a Prime; 
+	echo $n is not a prime;
+	exit; 
 elif [ $n -eq 2 -o $n -eq 3 ] 
 then 
-	echo Is Prime; 
+	echo $n is prime;
+	exit; 
 fi
 
+##Logic for rest of the numbers
 
+flag=true;
 for (( i = 2; i<=$(($n/2)) ; i++))
 do
 	if [ $(($n % $i)) -eq 0 ]
 	then 
-		echo Not a Prime
-		exit
+		echo $n is not a prime
+		flag=false
+		break;
 	fi
-	echo Is Prime
 done
-
+	
+if [ "$flag" = true ]
+then 
+	echo $n is prime;
+fi
 
